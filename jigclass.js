477e96e0104b1495607b22dec4ed536c93e6f8e8
saichaya <85267262+saichaya@users.noreply.github.com>
2021-06-14 17:31:06 -0400

    Add files via upload

--- jigclass.js
+++ jigclass.js
@@ -24,6 +24,12 @@ class Site extends Jig {
     setEmbed(html){
         this.embed = html
     }
+    useTipWidget(boolean){
+        this.tipwidget = boolean
+    }
+    setTipTo(tipto){
+        this.tipto = tipto
+    }
     setCSS(cssjig){
         this.css = cssjig
     }
@@ -38,5 +44,5 @@ class Site extends Jig {
   CSS.metadata = {emoji:'👔'}
   
 
-  //Site location 068b8fd258aaf47bb18907f728929d82437cb385001f313116bb61d2f4883cd9_o1
+  //Site location 30faeff157ba95c27e65feb1b0f7b849713ae732c3326935efdcad6ec590d44b_o1
   //CSS location 75210f54ee4c219cb381d0adc639c53e2cd206d93d28d3be49647904c4570ec2_o1
